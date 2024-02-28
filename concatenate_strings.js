b17fefaeff55fb23887b16aa9f255b614159d18b
SShipon <nxshipon@gmail.com>
2024-02-28 00:53:51 -0800

    number method and variables and concate strings

---
+++ concatenate_strings.js
@@ -0,0 +1,23 @@
+/* 
+স্ট্রিং মানগুলির concat() পদ্ধতিটি এই স্ট্রিংটিতে স্ট্রিং
+আর্গুমেন্টগুলিকে সংযুক্ত করে এবং একটি নতুন স্ট্রিং
+ প্রদান করে।
+*/
+const MyName = "Kamrul Islam "
+console.log("My Name Is " + MyName + "Shipon")
+
+/* 
+স্ট্রিং মানগুলির concat() পদ্ধতিটি এই স্ট্রিংটিতে স্ট্রিং
+আর্গুমেন্টগুলিকে সংযুক্ত করে এবং একটি নতুন স্ট্রিং
+ প্রদান করে।
+*/
+// My Heart Aripta 
+const name = "Tasmina Hoque ";
+const lastName = "Arpita" ;
+const age = 17;
+const color = "Black";
+const food ="Fast food";
+const profession ="Student";
+console.log("My name Is " + name + ' '+ lastName + " How Old Are You" + ' '
++ age +" favorite Color "+ color + " favorite food "
++ food + " I am " + profession);

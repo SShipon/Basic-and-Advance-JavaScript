b62a7ac756ebfaf448377013c4334050554ca482
SShipon <nxshipon@gmail.com>
2024-02-28 10:47:24 -0800

    push method and pop metheod js problem solved

---
+++ push.js
@@ -0,0 +1,9 @@
+const  number =[23, 455, 565, 66, 34, 67, 56,]
+const myNumber = number.push(566)
+console.log(number)
+console.log(myNumber)// add last element number add push method
+
+const name  =["Mim","Mina", "Hamim","Jubayer", "Nafee","Salauddin"] // add last string element add push method 
+const myName = name.push("Habib")
+console.log(name)
+console.log(myName)

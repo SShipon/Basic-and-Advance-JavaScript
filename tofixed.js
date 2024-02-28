f93e67e7a4f27135701379754eadbf2a6120d53b
SShipon <nxshipon@gmail.com>
2024-02-28 10:34:38 -0800

    tofixes add

---
+++ tofixed.js
@@ -0,0 +1,8 @@
+const number = 7.895;
+const result = number.toFixed()
+console.log(result)
+
+const myNum = 11.569
+const totalValue = myNum.toPrecision(2)
+const final = parseFloat(totalValue)
+console.log(final)

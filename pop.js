45050551fd947df0e4d2e95f830bb76ff5b170c9
SShipon <nxshipon@gmail.com>
2024-02-28 01:02:13 -0800

    javaScript method pop()

---
+++ pop.js
@@ -0,0 +1,8 @@
+const num =[12, 34, 35, 78, 22, 66, 70, 120] //number in the array
+num.pop()
+console.log(num) // array last element remove 120 javaScript Pop method use
+
+// string in the array 
+const name = ["shipon", "Lobna Akter", "Arif", "Jidan", "Mir Hossain", "Milton", "Arpita"]
+name.pop()
+console.log(name) // array last element remove Arpita get loss Pop Method use
